9a2a3b88350056836150aedd82431bab22ddb968
inchingforward <inchingforward@gmail.com>
2014-06-19 19:57:42 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject eon "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "eon"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "eon.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/eon/core.cljs
@@ -0,0 +1,5 @@
+(ns eon.core)
+
+(enable-console-print!)
+
+(println "Hello world!")
