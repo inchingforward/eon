6b16a9594f55421aa5d6eb7b9e3ecb67bde95cea
inchingforward <inchingforward@gmail.com>
2014-07-22 19:29:48 -0500

    Added tests for expected level and question keys.

--- test/eon/levels_test.cljs
+++ test/eon/levels_test.cljs
@@ -3,11 +3,21 @@
   (:require [cemerick.cljs.test :as t]
             [eon.levels :as levels]))
 
-(deftest level-contains-expected-keys
-  (let [level (levels/make-level 1)]
-    (is (contains? level :level))
-    (is (contains? level :questions))
-    (is (contains? level :title))
-    (is (contains? level :question-fn))
-    (is (contains? level :curr-question))
-    (is (contains? level :level))))
+(defn contains-keys? [m keys]
+  (every? #(contains? m %) keys))
+
+(defn questions-contain-keys? [m keys]
+  (every? #(contains-keys? % keys) (:questions m)))
+
+(def level-maps [(levels/make-level 1)
+                 (levels/make-level 2)
+                 (levels/make-level 3)
+                 (levels/make-level 4)])
+
+(deftest levels-contain-expected-keys
+  (let [keys [:level :questions :title :question-fn :curr-question :level]]
+    (is (every? #(contains-keys? % keys) level-maps))))
+
+(deftest questions-contain-expected-keys
+  (let [keys [:question :answer :answered? :points]]
+    (is (every? #(questions-contain-keys? % keys) level-maps))))
