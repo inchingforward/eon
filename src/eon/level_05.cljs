7c8ea7bc1137df20383ac963a591715231158354
inchingforward <inchingforward@gmail.com>
2014-08-10 13:25:20 -0500

    Added level 5 to the game.

--- src/eon/level_05.cljs
+++ src/eon/level_05.cljs
@@ -1,41 +1,21 @@
 (ns eon.level-05
-  "Arithmetic.")
+  "Sequence Equality.")
 
-(defn make-add-question []
-  (let [num1 (rand-int 10)
-        num2 (rand-int 10)]
-    {:question (str "(+ " num1 " " num2 ")")
-     :answer   (+ num1 num2)}))
-
-(defn make-sub-question []
-  (let [num1 (rand-int 10)
-        num2 (rand-nth (range (inc num1)))]
-    {:question (str "(- " num1 " " num2 ")")
-     :answer   (- num1 num2)}))
-
-(defn make-mul-question []
-  (let [num1 (rand-int 10)
-        num2 (rand-int 10)]
-    {:question (str "(* " num1 " " num2 ")")
-     :answer   (* num1 num2)}))
-
-(defn make-div-question []
-  (let [num1 (rand-nth [1 2 3 4 5 6 7 8 9])
-        num2 (rand-nth [1 2 3 4 5 6 7 8 9])
-        prod (* num1 num2)]
-    {:question (str "(/ " prod " " num1 ")")
-     :answer   num2}))
-
-(def arith-fns [make-add-question, make-sub-question, make-mul-question, make-div-question])
-
-(defn make-question []
-  (let [question-fn (rand-nth arith-fns)]
-    (merge (question-fn) {:answered? false :points 100})))
+(def qs [{:question "(= () '())" :answer true}
+         {:question "(= [] [])" :answer true}
+         {:question "(= () [])" :answer true}
+         {:question "(= [1 2 3] '(1 2 3))" :answer true}
+         {:question "(= () [] {})" :answer false}
+         {:question "(= [1 2 3] [(+ 1 0) (+ 2 0) (+ 3 0)])" :answer true}
+         {:question "(= (cons 1 (cons 2 (cons 3 '()))) (1 2 3))" :answer true}
+         {:question "(= (cons 1 (cons 2 (cons 3 '()))) [1 2 3])" :answer true}
+         {:question "(= (conj (conj (conj [] 1) 2) 3))" :answer true}
+         {:question "(= [1 2 3] (conj (conj (conj [] 3) 2) 1))" :answer false}])
 
 (defn make-questions [num-questions]
-  (vec (take num-questions (repeatedly make-question))))
+  (vec (take num-questions (map #(merge % {:answered? false :points 100}) qs))))
 
 (defn make-level [num-questions]
   {:level 5
-   :title "Arithmetic"
+   :title "Sequence Equality"
    :questions (make-questions num-questions)})
--- src/eon/levels.cljs
+++ src/eon/levels.cljs
@@ -3,11 +3,13 @@
   (:require [eon.level-01 :as level-01]
             [eon.level-02 :as level-02]
             [eon.level-03 :as level-03]
-            [eon.level-04 :as level-04]))
+            [eon.level-04 :as level-04]
+            [eon.level-05 :as level-05]))
 
 (defn make-levels [num-questions]
   "Builds a vector of level maps."
   [(level-01/make-level num-questions)
    (level-02/make-level num-questions)
    (level-03/make-level num-questions)
-   (level-04/make-level num-questions)])
+   (level-04/make-level num-questions)
+   (level-05/make-level num-questions)])
