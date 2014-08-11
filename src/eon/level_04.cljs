6450b6904cd0b949408ffc1e7a788a4ef87db163
mj <inchingforward@gmail.com>
2014-08-11 12:40:41 -0500

    Added more questions.

--- src/eon/level_04.cljs
+++ src/eon/level_04.cljs
@@ -1,7 +1,10 @@
 (ns eon.level-04
   "Scalar Equality.")
 
-(def qs [{:question "(= 1 (+ 0 1) (- 5 4) (/ 9 9))" :answer true}
+(def qs [{:question "(= 1 1)" :answer true}
+         {:question "(= 3.14 3.14)" :answer true}
+         {:question "(= 3.14 3.1400)" :answer true}
+         {:question "(= 1 (+ 0 1) (- 5 4) (/ 9 9))" :answer true}
          {:question "(= \"a\" \\a)" :answer false}
          {:question "(= \"a\" (str \\a))" :answer true}
          {:question "(= :a \"a\")" :answer false}
@@ -10,7 +13,8 @@
          {:question "(= \"hello\" (str [\\h \\e \\l \\l \\o]))" :answer false}
          {:question "(= \"hello\" (apply str [\\h \\e \\l \\l \\o]))" :answer true}
          {:question "(= \"hello\" (str \"hell\" \"o\"))" :answer true}
-         {:question "(identical? \"hello\" (str \"hell\" \"o\"))" :answer false}])
+         {:question "(identical? \"hello\" (str \"hell\" \"o\"))" :answer false}
+         {:question "(identical? \"hello\" \"hello\"))" :answer true}])
 
 (defn make-questions [num-questions]
   (vec (take num-questions
