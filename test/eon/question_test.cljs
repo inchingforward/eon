00b6fb6f1bc8a2d18a6c23ae54c8984d43f88228
mj <inchingforward@gmail.com>
2014-08-26 12:50:52 -0500

    Added Question test.

--- test/eon/question_test.cljs
+++ test/eon/question_test.cljs
@@ -2,3 +2,10 @@
   (:require-macros [cemerick.cljs.test :refer [deftest is]])
   (:require [cemerick.cljs.test :as t]
             [eon.question :as q]))
+
+(deftest sanity-test
+  (let [question (q/make-question "Question" "Answer")]
+    (is (= "Question" (:question question)))
+    (is (= "Answer" (:answer question)))
+    (is (= false (:answered? question)))
+    (is (= 100 (:points question)))))
