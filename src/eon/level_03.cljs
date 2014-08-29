2cb822a6e14cc134aacff5001b2a7eaba9b768cb
mj <inchingforward@gmail.com>
2014-08-29 13:50:20 -0500

    Question fixes and additions.

--- src/eon/level_03.cljs
+++ src/eon/level_03.cljs
@@ -3,21 +3,23 @@
   (:require [eon.question :refer [make-question shuffle-and-take]]))
 
 (def qs [(make-question "(nil? nil)" true)
+         (make-question "(nil? \"\")" false)
          (make-question "(nil? true)" false)
          (make-question "(nil? false)" false)
          (make-question "nil" "nil")
-         (make-question "(not nil)" true)
          (make-question "(nil? '(nil))" false)
          (make-question "(nil? '())" false)
          (make-question "(or nil nil)" "nil")
          (make-question "(or nil true)" true)
-         (make-question "(seq? '())" true)
          (make-question "(seq '())" "nil")
          (make-question "(seq [])" "nil")
          (make-question "(nil? (seq '()))" true)
          (make-question "(nil? (seq []))" true)
          (make-question "(nil? (first '(nil)))" true)
-         (make-question "(if '() true false)" true)])
+         (make-question "(nil? (first []))" true)
+         (make-question "(if '() true false)" true)
+         (make-question "(if [] true false)" true)
+         (make-question "(if {} true false)" true)])
 
 (defn make-level [num-questions]
   {:level 3
--- src/eon/level_05.cljs
+++ src/eon/level_05.cljs
@@ -5,14 +5,14 @@
 (def qs [(make-question "(= () '())" true)
          (make-question "(= [] [])" true)
          (make-question "(= () [])" true)
+         (make-question "(= () [] {})" false)
          (make-question "(= [1 2 3] '(1 2 3))" true)
          (make-question "(= [1 2 3] #{1 2 3))" false)
-         (make-question "(= () [] {})" false)
          (make-question "(= [1 2 3] [(+ 1 0) (+ 2 0) (+ 3 0)])" true)
-         (make-question "(= (cons 1 (cons 2 (cons 3 '()))) (1 2 3))" true)
-         (make-question "(= (cons 1 (cons 2 (cons 3 '()))) [1 2 3])" true)
-         (make-question "(= [1 2 3] (conj (conj (conj [] 3) 2) 1))" false)
-         (make-question "(= [3 2 1] (conj (conj (conj [] 3) 2) 1))" true)])
+         (make-question "(= (cons 1 (cons 2 '())) '(1 2))" true)
+         (make-question "(= (cons 1 (cons 2 '())) [1 2])" true)
+         (make-question "(= [1 2] (conj (conj [] 2) 1))" false)
+         (make-question "(= [2 1] (conj (conj [] 2) 1))" true)])
 
 (defn make-level [num-questions]
   {:level 5
